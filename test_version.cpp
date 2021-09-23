4e05ccf14b2a6624c5d9a8f5a7f9db61d8db38d6
Igor <ivahotin@gmail.com>
2021-09-23 11:04:04 +0300

    Add google test

--- test_version.cpp
+++ test_version.cpp
@@ -1,14 +1,12 @@
-#define BOOST_TEST_MODULE test_version
+#include <gtest/gtest.h>
 
 #include "lib.h"
 
-#include <boost/test/unit_test.hpp>
-
-BOOST_AUTO_TEST_SUITE(test_version)
-
-BOOST_AUTO_TEST_CASE(test_valid_version)
-{
-    BOOST_CHECK(version() > 0);
+TEST(TestTest, EmptyTest) {
+    EXPECT_GT (version(), 0);
 }
 
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
